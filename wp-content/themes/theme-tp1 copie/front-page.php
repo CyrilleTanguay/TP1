2d8a5939e54c8d1f1aea8354fd4757c47d52a374
CyrilleTanguay <e1974422@cmaisonneuve.qc.ca>
2021-03-15 17:52:37 -0400

    Ajout de 2 autres slides au carrousel et de leurs liens

--- wp-content/themes/theme-tp1 copie/front-page.php
+++ wp-content/themes/theme-tp1 copie/front-page.php
@@ -23,14 +23,18 @@ get_header();
             <?php
 		if(is_front_page()) : ?>
 				<section class="carrousel">
-				<div>1</div>
-				<div>2</div>
-				<div>3</div>
+				<div><a href="http://localhost:8080/TP/category/image/">Imagerie 3D</a></div>
+				<div><a href="http://localhost:8080/TP/category/cours/">Cours</a></div>
+				<div><a href="http://localhost:8080/TP/category/jeu/">Jeu</a></div>
+				<div><a href="http://localhost:8080/TP/category/specifique/">Spécifique</a></div>
+				<div><a href="http://localhost:8080/TP/category/web/">Web</a></div>
 				</section>
                 <div style="display: flex; justify-content:center;">
 				<button id='un'>1</button>
 				<button id='deux'>2</button>
 				<button id='trois'>3</button>
+				<button id='quatre'>4</button>
+				<button id='cinq'>5</button>
                 </div>
 				<?php endif ?>
 			<?php
