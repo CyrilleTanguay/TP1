94e33844b2b3002b4e8df6276bae9485413db5d9
CyrilleTanguay <e1974422@cmaisonneuve.qc.ca>
2021-03-21 22:47:57 -0400

    Modification dans le carrousel et le pied de page

--- wp-content/themes/theme-tp1 copie/footer.php
+++ wp-content/themes/theme-tp1 copie/footer.php
@@ -13,17 +13,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'theme-tp1' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				// printf( esc_html__( 'Proudly powered by %s', 'theme-tp1' ), 'WordPress' );
-				?>
-			</a>
-			<!-- <span class="sep"> | </span> -->
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				// printf( esc_html__( 'Theme: %1$s by %2$s.', 'theme-tp1' ), 'theme-tp1', '<a href="http://underscores.me/">Cyrille Tanguay</a>' );
-				?>
+		<p style="text-align:center"> Ce TP1 contient des descriptions de différents cours du TIM.</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
